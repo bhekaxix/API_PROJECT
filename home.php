4e539b5ee87a0d379fec28ac43dcd2d6608cfe7d
Taonga Bheka <taongabp@gmail.com>
2025-02-04 09:01:49 +0300

    modified

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet"  href="home.css">
+	<link rel="stylesheet" type="text/css" href="/api_project/css/home.css">
 	<title>Oil Refinery</title>
 </head>
 <body>
