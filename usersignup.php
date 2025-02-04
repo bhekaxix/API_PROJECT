93b12a2f7d6b6b800db440e6d32a52ab7a0bcfc3
Taonga Bheka <taongabp@gmail.com>
2025-02-04 06:37:11 +0300

    usersign up

---
+++ usersignup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="signup.css">
+	<script src="signup.js"></script>
+
+	<title>User Sign Up</title>
+</head>
+<body>
+	<div class="nav">
+		<div class="logo"><h2>oIlR</h2></div>
+			<div class="navlinks">
+	<ul>
+		<li><a href="home.php" class="navborder">Home</a></li>
+		<li><a class="navborder" href="#">About Us</a></li>
+		<li><a href="#" class="navborder">Contact Us</a></li>
+  </ul>
+ </div>
+</div>
+
+<div class="container">
+	<form class="signup-form">
+		<h2 style="color: crimson;">Sign up as:</h2>
+		<hr>
+		<br>
+		
+		<button class="signup"><a href="signup.php">Customer</a></button>
+		<button class="signup"><a href="asignup.php">Administrator</a></button>
+		<button class="signup"><a href="dsignup.php">Dispatcher</a></button>
+		
+		
+		<br>
+		<br>
+		
+
+	</form>
+		
+		
+			
+		
+</body>
+</html>
